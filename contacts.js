76f7dfcf02bd10294ec7f11f91768ad36a9020d5
Yurii <gapirov21@gmail.com>
2024-01-16 15:23:37 +0200

    fix some comment

--- contacts.js
+++ contacts.js
@@ -1,8 +1,3 @@
-// У файл contacts.js зроби імпорт модулів fs (у версії, яка працює з промісами - fs/promises) і path для роботи з файловою системою.
-// Створи змінну contactsPath і запиши в неї шлях до файлу contacts.json. Для складання шляху використовуй методи модуля path.
-// Додай асинхронні функції для роботи з колекцією контактів. У функціях використовуй модуль fs та його методи readFile() і writeFile(). Відповідні функції мають повертати необхідні дані за допомогою оператора return. Вивід в консоль в написаних функціях здійснюватись не повинен.
-// Зроби експорт створених функцій.
-
 const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
@@ -10,20 +5,17 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db", "contacts.json"); //пас джоін обєднання шляху до поточного каталогу, папки і імелі файлу
 
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
   const data = await fs.readFile(contactsPath, "utf-8"); //parse сам робе ютф-8, можем не прописувати
   return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await listContacts();
   const contact = contacts.find((item) => item.id === contactId);
   return contact || null;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === contactId); //пошук книжки яку треба видалити
   if (index === -1) return null; //якщо книжку не знайшли повернули нал
@@ -33,7 +25,6 @@ async function removeContact(contactId) {
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
   const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
--- index.js
+++ index.js
@@ -17,47 +17,31 @@ const {
   removeContact,
 } = require("./contacts");
 
-// // TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
       const allContacts = await listContacts();
       console.table(allContacts);
       break;
 
     case "get":
-      // ... id
       const contact = await getContactById(id);
       console.table(contact);
       break;
 
     case "add":
-      // ... name email phone
       const newContact = await addContact(name, email, phone);
       console.table(newContact);
       break;
 
     case "remove":
-      // ... id
       const removedContact = await removeContact(id);
       console.table(removedContact);
-
       break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
-
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "e6ywwRe4jcqxXfCZOj_1e3" });
-// invokeAction({
-//   action: "add",
-//   name: "Yurii Gapirov",
-//   email: "gasdas@mail.com",
-//   phone: "123456789",
-// });
-// invokeAction({ action: "remove", id: "-RrkV7v_T_N8XBcoyY5Ww" });
 
 invokeAction(options);
